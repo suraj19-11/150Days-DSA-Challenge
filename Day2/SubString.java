2cfcbf44cb683aea2737818a4b68a8a686d051f0
Suraj sah <sah19112004@gmail.com>
2025-12-10 21:15:08 +0530

    Solve using StringBuilder Method

--- Day2/SubString.java
+++ Day2/SubString.java
@@ -7,10 +7,20 @@ public class SubString {
         return substr;
 
     }
+
+    //Using String Builder
+    public static String stb(String str, int s, int e){
+        StringBuilder sb = new StringBuilder("");
+        for(int i=s; i<e; i++){
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
     public static void main(String[] args) {
         String str = "HelloWorld";
         System.out.println(SS(str, 1,5));
-        System.out.println(str.substring(2,7));
+        System.out.println(stb(str, 0,5));  //StringBuilder Method
+        System.out.println(str.substring(2,7));  //Direct function for substring
     }
 }
 
